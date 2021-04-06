1992f3416f912368197ff8c3badddbc1684b6b9b
Vadim <vraspopin@inbox.ru>
2021-04-06 17:12:12 +0300

    Урок 11

---
+++ app/src/main/java/ru/varasoft/kotlin/kotlinlesson9/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,21 @@
+package ru.varasoft.kotlin.kotlinlesson9
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.messaging.Constants.TAG
+
+
+class MyFirebaseInstanceIdService :  FirebaseInstanceIdService() {
+
+    override fun onTokenRefresh() {
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: $refreshedToken")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken)
+    }
+}
